923aba6a1605a749f9d4eaf9418fafac7877f13b
dumaevrinat <dumaevrinat@gmail.com>
2020-07-27 17:57:14 +0300

    change Router

--- shnotes_frontend/src/index.js
+++ shnotes_frontend/src/index.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import './index.css'
-import {BrowserRouter} from 'react-router-dom'
+import {HashRouter} from 'react-router-dom'
 import App from "./Components/App"
 
 import {Provider} from 'react-redux'
@@ -10,9 +10,9 @@ import store from './store'
 
 ReactDOM.render(
     <Provider store={store}>
-        <BrowserRouter>
+        <HashRouter>
             <App/>
-        </BrowserRouter>
+        </HashRouter>
     </Provider>,
     document.getElementById('root')
 )
