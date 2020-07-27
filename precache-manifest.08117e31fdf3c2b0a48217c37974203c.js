4d0b673cdeeb0e5a99866e10d80733e2002ec291
dumaevrinat <dumaevrinat@gmail.com>
2020-07-27 14:47:47 +0000

    Updates

---
+++ precache-manifest.08117e31fdf3c2b0a48217c37974203c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "979d8b4c3ae59b05b25db5379bab20ea",
+    "url": "/shnotes/index.html"
+  },
+  {
+    "revision": "b4ee3885a6a590905734",
+    "url": "/shnotes/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "49b73b20d93ea85b5899",
+    "url": "/shnotes/static/js/2.0858d026.chunk.js"
+  },
+  {
+    "revision": "f0eca9bad9d0594c354a11d0063f31f5",
+    "url": "/shnotes/static/js/2.0858d026.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b4ee3885a6a590905734",
+    "url": "/shnotes/static/js/main.216ff4d1.chunk.js"
+  },
+  {
+    "revision": "8dadf90fed7da4f2ccdc",
+    "url": "/shnotes/static/js/runtime-main.16645680.js"
+  }
+]);
