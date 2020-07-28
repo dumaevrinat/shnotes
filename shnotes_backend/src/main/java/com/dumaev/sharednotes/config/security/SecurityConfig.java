6936893f669f8dd5bfd96b3fdce0d10210dd853f
dumaevrinat <dumaevrinat@gmail.com>
2020-07-28 23:11:56 +0300

    add ssl config

--- shnotes_backend/src/main/java/com/dumaev/sharednotes/config/security/SecurityConfig.java
+++ shnotes_backend/src/main/java/com/dumaev/sharednotes/config/security/SecurityConfig.java
@@ -1,14 +1,17 @@
-//package com.dumaev.sharednotes.config.security;
-//
-//import org.springframework.context.annotation.Bean;
-//import org.springframework.context.annotation.Configuration;
-//import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-//import org.springframework.security.crypto.password.PasswordEncoder;
-//
-//@Configuration
-//public class SecurityConfig {
-//    @Bean
-//    public PasswordEncoder passwordEncoder() {
-//        return new BCryptPasswordEncoder();
-//    }
-//}
+package com.dumaev.sharednotes.config.security;
+
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .requiresChannel()
+                .anyRequest()
+                .requiresSecure();
+    }
+}
