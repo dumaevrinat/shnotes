640c77b26322eba8006479cf723a29d4a43f4771
dumaevrinat <dumaevrinat@gmail.com>
2020-07-29 16:16:53 +0300

    update security config

--- shnotes_backend/src/main/java/com/dumaev/sharednotes/config/security/SecurityConfig.java
+++ shnotes_backend/src/main/java/com/dumaev/sharednotes/config/security/SecurityConfig.java
@@ -3,6 +3,7 @@ package com.dumaev.sharednotes.config.security;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
 
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
@@ -10,6 +11,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
+                .csrf().disable()
                 .requiresChannel()
                 .anyRequest()
                 .requiresSecure();
