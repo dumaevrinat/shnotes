3b8a2faaa2f4be6d431bde8f138b614450006d44
dumaevrinat <dumaevrinat@gmail.com>
2020-08-03 13:26:19 +0300

    change ids

--- database/src/main/resources/db/migration/V1__create_database.sql
+++ database/src/main/resources/db/migration/V1__create_database.sql
@@ -1,42 +1,37 @@
 create table notebooks
 (
-    string_id varchar(255) not null,
-    name varchar(255) not null,
-    constraint string_id
-        unique (string_id)
+    id binary(16) not null default (uuid_to_bin(uuid())) primary key,
+    name varchar(255) not null
 );
-
-alter table notebooks
-    add primary key (string_id);
 
 create table notes
 (
-    id int auto_increment primary key,
+    id binary(16) not null default (uuid_to_bin(uuid())) primary key,
     text blob null,
     is_done tinyint(1) not null,
-    notebook_string_id varchar(255) not null,
+    notebook_id binary(16) not null,
     title varchar(255) null,
     is_high_priority tinyint(1) not null,
     constraint notes_ibfk_1
-        foreign key (notebook_string_id) references notebooks (string_id)
+        foreign key (notebook_id) references notebooks (id)
             on delete cascade
 );
 
 create table task_lists
 (
-    id int auto_increment primary key,
-    notebook_string_id varchar(255) not null,
+    id binary(16) not null default (uuid_to_bin(uuid())) primary key,
+    notebook_id binary(16) not null,
     title varchar(255) null,
     is_high_priority tinyint(1) not null,
     constraint task_lists_ibfk_1
-        foreign key (notebook_string_id) references notebooks (string_id)
+        foreign key (notebook_id) references notebooks (id)
             on delete cascade
 );
 
 create table tasks
 (
-    id int auto_increment primary key,
-    task_list_id int not null,
+    id binary(16) not null default (uuid_to_bin(uuid())) primary key,
+    task_list_id binary(16) not null,
     text blob null,
     is_done tinyint(1) not null,
     constraint tasks_ibfk_1
